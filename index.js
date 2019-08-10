481898c2d4a4d1d8091c02a0f6956064a11229f0
andrew <andrewjthomsen@yahoo.com>
2019-08-10 14:13:35 -0700

    Transfered work form old repo to current repo

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // write your code here
+const http = require("http");
+
+// write your code here
+  const server = http.createServer((req, res) => {
+  console.log("Received" + req.method + " request for: " + req.url);
+
+  res.writeHead(200, { "content-type": "text/plain" });
+
+  res.end("Hello world!");
+});
+
+server.listen(8080, "localhost", null, function() {
+  console.log("Server is listening on localhost:8080");
+});
